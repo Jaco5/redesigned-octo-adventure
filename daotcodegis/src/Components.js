79a1359963c67a6742e9f33c1958c8c831b63079
Jake <jacobartz1987@gmail.com>
2019-11-29 20:10:17 -0700

    user input for operative string, initial display

--- daotcodegis/src/Components.js
+++ daotcodegis/src/Components.js
@@ -5,7 +5,11 @@ import React from "react";
 export default class Form extends React.Component {
     state = {
         string : "th",
-        inputText : "A bunch of the words. A bunch of the words. A bunch of the words. A bunch of the words. A bunch of the words.",
+        inputText : "Hello user. \n\n This text area here is where you input the body of text you would like to parse. The 'Operative String' is the combination and sequence of characters that you want to look for.",
+        ocurrences : 0,
+        words : ['a','b',"c"],
+        sentences : ['a','b',"c"]
+
     };
 
     _regExpEscape = (stringInput) => {
@@ -13,77 +17,84 @@ export default class Form extends React.Component {
     };
    
 
-    _countString =()=> {
+    _countString =(event)=> {
+        event.preventDefault();
         let regExp = new RegExp(this._regExpEscape(this.state.string), "ig");
         console.log(regExp);
         let strArr = this.state.inputText.match(regExp);
         console.log(strArr);
         if (strArr !== null) {
             let count = strArr.length;
+            this.setState({ ocurrences:count })
             console.log(count)
         } else {
             console.log("Yo, its null.")
         }
     }
-//\b\w*[Yy]\w*\b/
-    _wordsWithString =()=> {
-        // let regString = "\\b\\w*"+(this._regExpEscape(this.state.string))+"\\w*\\b"
+    _wordsWithString =(event)=> {
+        event.preventDefault();
         let regExp = new RegExp("\\b\\w*"+(this._regExpEscape(this.state.string))+"\\w*\\b", "ig");
         console.log(regExp);
         let strArr = this.state.inputText.match(regExp);
+        this.setState({ words:strArr })
         console.log(strArr);
         if (strArr !== null) {
             let count = strArr.length;
+            this.setState({ ocurrences:count })
             console.log(count)
         } else {
             console.log("Yo, its null.")
         }
     }
-    //  [A-Za-z," ]+word[A-Za-z," ]+
-    // ***deprecated***
-    // _sentencesWithWord =()=> {
-    //     //let regExp = new RegExp('[A-Za-z,\" ]'+(this._regExpEscape(this.state.string))+'[A-Za-z,\" ]+', "ig"); //pulls clauses
-    //     let regExp = new RegExp('[^.?!]*(?<=[.?!\\s])'+(this._regExpEscape(this.state.string))+'(?=[\\s.?!])[^.?!]*[.?!]', 'ig'); // pulls on case sensitive word
-    //     console.log(regExp);
-    //     let strArr = this.state.inputText.match(regExp);
-    //     console.log(strArr);
-    //     let count = strArr.length;
-    //     console.log(count)
-    // }
-    _sentencesWithString =()=> {
+    _sentencesWithString =(event)=> {
+        event.preventDefault();
         let regExp = new RegExp('[^.?!]*(?<=[.?!\\s])\\b\\w*'+(this._regExpEscape(this.state.string))+'\\w*\\b(?=[\\s.?!])[^.?!]*[.?!]', 'ig'); 
         console.log(regExp);
         let strArr = this.state.inputText.match(regExp);
+        this.setState({ sentences:strArr })
         console.log(strArr);
         if (strArr !== null) {
             let count = strArr.length;
+            this.setState({ ocurrences:count })
             console.log(count)
         } else {
             console.log("Yo, its null.")
-        };
+        }
        
     }
     render() {
         return (
-            <div>
-                <form >
-                    <label>
-                        Text to parse:
-                        <textarea type="text" className="main-text-input"
-                        value={this.state.inputText} 
-                        onChange={e=>this.setState({inputText:e.target.value})}
-                        />
-                    </label>
+            <div className="main-div">
+                <form className="main-form" >
+                    <div id="a">
+                        <label htmlFor="mti" className="text-inputlabel">Text to parse:</label>
+                        <textarea type="text" name="mti" className="text-input" 
+                            value={this.state.inputText} 
+                            onChange={e=>this.setState({inputText:e.target.value})}
+                            />
+                        </div>
+                    <div id="b">
+                        <label htmlFor="msi" className="string-input-label">Operative String: </label>
+                        <input type="text" name="msi" className="string-input"
+                            value={this.state.string}
+                            onChange={e=>this.setState({string:e.target.value})} 
+                            />
+                        <label htmlFor="count">Count the occurences of your string within the text.</label>
+                        <button name="count" onClick={this._countString}>Count!</button>
+                        <label htmlFor="words">Find all the words your string is used in, if your string is a word, this will return any longer words that have recently eaten it.</label>
+                        <button name="words" onClick={this._wordsWithString}>Words!</button>
+                        <label htmlFor="sentences">This will return all the sentences that your string occurs in.</label>
+                        <button name="sentences"onClick={this._sentencesWithString}>Sentences!</button>
+                    </div>
                     
                 </form>
-                <button onClick={this._countString}>Count!</button>
-                <button onClick={this._wordsWithString}>Words!</button>
-                <button onClick={this._sentencesWithWord}>"Sentences(Word)!"</button>
-                <button onClick={this._sentencesWithString}>"Sentences!(String)"</button>
+                <div id="c">
+                        <p>Ocurrences: {this.state.ocurrences}</p>
+                        <p>Words: {this.state.words}</p>
+                        <p>Sentences: {this.state.sentences}</p>
+                </div>
+                
             </div>
         );
     }
 }
-
-
-
